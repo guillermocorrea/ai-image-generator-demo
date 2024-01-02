7b1350581cd8b7af1385beb7e368670c059a7cf8
Guillermo Correa <guillermo.correa.99@gmail.com>
2024-01-02 11:43:14 +0000

    Initial commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function LandingPage() {
+  return (
+    <>  
+      <p className="text-6xl">Landing Page</p>
+      <Button>Button</Button>
+    </>
+  )
+}
